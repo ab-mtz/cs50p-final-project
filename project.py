dde7c842b280dc5d65372d4698468c8f936f1670
ab-mtz <bmtzs@me.com>
2023-10-12 23:46:48 +0200

    events

--- project.py
+++ project.py
@@ -1,8 +1,22 @@
-import tabula
+# import tabula
+from ics import Calendar, Event
 
 
-path = "sample.pdf"
+# path = "sample.pdf"
 
-table = tabula.read_pdf(path, pages=1)
+# table = tabula.read_pdf(path, pages=1)
 
-print(table[0])
+# print(table[0])
+
+### create a New calendar and add events
+
+c = Calendar()
+e = Event()
+e.name = "Work"
+e.begin = '2023-10-13 10:00:00'
+e.events.end = '2023-10-13 18:00:00'
+
+c.events.add(e)
+c.events
+
+print(event)
