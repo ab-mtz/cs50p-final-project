e61e2bd0ed5b66a62018af1e0fc16850258d0835
ab-mtz <bmtzs@me.com>
2023-10-14 18:52:56 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 # imports
 from icalendar import Calendar, Event, vCalAddress, vText, Alarm
 from datetime import datetime, timedelta
+from uuid import uuid1
 from pathlib import Path
 import os
 import pytz
@@ -18,34 +19,35 @@ cal.add('version', '2.0')
 event = Event()
 event.add('name', 'Work')
 event.add('description', 'V')
-event.add('dtstart', datetime(2023, 10, 14, 9, 0, 0, tzinfo=pytz.utc))
+event.add('dtstart', datetime(2023, 10, 14, 18, 45, 0, tzinfo=pytz.utc))
 event.add('dtend', datetime(2023, 10, 14, 19, 0, 0, tzinfo=pytz.utc))
-event.add_component(event)
+
+cal.add_component(event)
 
 # Creatinga alarm
 
-### working yet ###
+### Not working yet ###
 alert = 20
-if alert and unicode(alert).isnumeric()
-    alarm = Alarm()
-    alarm.add('action', 'DISPLAY')
+# if alert:
+alarm = Alarm()
+alarm.add('action', 'DISPLAY')
 
-    alert_time = timedelta(minutes=-int(alert))
-    alarm.add('trigger', alert_time)
+alert_time = timedelta(minutes=-int(alert))
+alarm.add('trigger', alert_time)
 
-    event.add_component(alarm)
+cal.add_component(alarm)
 # c.events
 
 # Write to disk
-directory = Path.cwd() / 'MyCalendar'
-try:
-   directory.mkdir(parents=True, exist_ok=False)
-except FileExistsError:
-   print("Folder already exists")
-else:
-   print("Folder was created")
+# directory = Path.cwd() / 'MyCalendar'
+# try:
+#    directory.mkdir(parents=True, exist_ok=False)
+# except FileExistsError:
+#    print("Folder already exists")
+# else:
+#    print("Folder was created")
  
-f = open(os.path.join(directory, 'example.ics'), 'wb')
+f = open(os.path.join('example.ics'), 'wb')
 f.write(cal.to_ical())
 f.close()
 
