3b238720a7361d564a6d1e308af988755fa9b0a3
ab-mtz <bmtzs@me.com>
2023-10-14 21:48:30 +0200

    Update project.py

--- project.py
+++ project.py
@@ -5,51 +5,55 @@ from uuid import uuid1
 from pathlib import Path
 import os
 import pytz
- 
-# Import from table
 
-# Extract and process info
+def main(): 
+    # Import from table
 
-# Save to calendar
+    # Extract and process info
+    out_path = 'cal_ex.ics'
+    event_name = 'Arbeit'
+    event_description = 'Verkauf'
+    start_time = datetime(2023, 10, 15, 8, 0, 0, tzinfo=pytz.utc)
+    end_time = datetime(2023, 10, 15, 10, 0, 0, tzinfo=pytz.utc)
+    alert = 1
+    events = [
+        create_event(event_name, event_description, start_time, end_time, alert)
+    ]
 
-### create a New calendar and add events
-# init the calendar
-cal = Calendar()
+    create_and_save_calendar(events, out_path)
+    # Save to calendar
 
-# Add properties
-cal.add('prodid', '-//My calendar product//example.com//')
-cal.add('version', '2.0')
+    ### create a New calendar and add events
+def create_event(name, description, start_time, end_time, alert):
+    event = Event()
+    event.add('summary', name)
+    event.add('description', description)
+    event.add('dtstart', start_time)
+    event.add('dtend', end_time)
+    if alert:
+        alarm = Alarm()
+        alarm.add('action', 'DISPLAY')
+        alert_time = timedelta(minutes=-int(alert))
+        alarm.add('trigger', alert_time)
+        event.add_component(alarm)
+    return event
 
+def create_and_save_calendar(events, out_path):
+    # init the calendar
+    cal = Calendar()
 
-# Add subcomponents
-week_shifts = []
+    # Add properties
+    cal.add('prodid', '-//My calendar product//example.com//')
+    cal.add('version', '2.0')
 
-event = Event()
-event.add('name', 'Schicht')
-event.add('description', 'V')
-event.add('dtstart', datetime(2023, 10, 14, 19, 10, 0, tzinfo=pytz.utc))
-event.add('dtend', datetime(2023, 10, 14, 21, 0, 0, tzinfo=pytz.utc))
+    for event in events:
+        cal.add_component(event)
+    # Store to file
+    
+    with open(out_path, 'wb') as file:
+        file.write(cal.to_ical())
 
-week_shifts.append(event)
-for shift in week_shifts:
-    cal.add_component(shift)
-
-# Creatinga alarm
-
-alert = 1
-alarm = Alarm()
-alarm.add('action', 'DISPLAY')
-
-alert_time = timedelta(minutes=-int(alert))
-alarm.add('trigger', alert_time)
-
-cal.add_component(alarm)
-
-
-# Store to file
- 
-f = open(os.path.join('example.ics'), 'wb')
-f.write(cal.to_ical())
-f.close()
+if __name__ == "__main__":
+    main()
 
 
