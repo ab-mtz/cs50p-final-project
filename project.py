d87883924452ce2f71cde23c709575e62370ff0b
ab-mtz <bmtzs@me.com>
2023-10-14 20:21:03 +0200

    Update project.py

--- project.py
+++ project.py
@@ -9,65 +9,43 @@ import pytz
 # Import from table
 
 # Extract and process info
-def main():
-    shift_data = 1
-    alert_data = 1
 
 # Save to calendar
+
 ### create a New calendar and add events
-    # init the calendar
-    cal = Calendar()
+# init the calendar
+cal = Calendar()
 
-    # Add properties
-    cal.add('prodid', '-//My calendar product//example.com//')
-    cal.add('version', '2.0')
-    shift = create_event(shift_data)
-    cal.add_component(shift)
-    alert = set_alarm(alert_data)
-    cal.add_component(alert) 
-    print(cal.content_lines)
+# Add properties
+cal.add('prodid', '-//My calendar product//example.com//')
+cal.add('version', '2.0')
 
-    # directory = Path.cwd() / 'MyCalendar'
-# try:
-#    directory.mkdir(parents=True, exist_ok=False)
-# except FileExistsError:
-#    print("Folder already exists")
-# else:
-#    print("Folder was created")
- 
- # Store to file
-    try:
-        f = open(os.path.join('example.ics'), 'wb')
-        f.write(cal.to_ical())
-        f.close()
-    except:
-        print("error saving file")
+
 # Add subcomponents
+event = Event()
+event.add('name', 'Schicht')
+event.add('description', 'V')
+event.add('dtstart', datetime(2023, 10, 14, 19, 10, 0, tzinfo=pytz.utc))
+event.add('dtend', datetime(2023, 10, 14, 21, 0, 0, tzinfo=pytz.utc))
 
-def create_event(s):
-    event = Event()
-    event.add('name', 'Schicht')
-    event.add('description', 'V')
-    event.add('dtstart', datetime(2023, 10, 14, 19, 10, 0, tzinfo=pytz.utc))
-    event.add('dtend', datetime(2023, 10, 14, 21, 0, 0, tzinfo=pytz.utc))
-    return
+cal.add_component(event)
 
-    # Creatinga alarm
-def set_alarm(a):
-    # if alert:
-    alert = a
-    alarm = Alarm()
-    alarm.add('action', 'DISPLAY')
-    alert_time = timedelta(minutes=-int(alert))
-    alarm.add('trigger', alert_time)
-    return
+# Creatinga alarm
 
-# read from calendar
+alert = 1
+alarm = Alarm()
+alarm.add('action', 'DISPLAY')
 
-# e = open('MyCalendar/example.ics', 'rb')
-# ecal = icalendar.Calendar.from_ical(e.read())
-# for component in ecal.walk():
-#    print(component.name)
-# e.close()
-# if __name__ == "__main__":
-    main()
+alert_time = timedelta(minutes=-int(alert))
+alarm.add('trigger', alert_time)
+
+cal.add_component(alarm)
+
+
+# Store to file
+ 
+f = open(os.path.join('example.ics'), 'wb')
+f.write(cal.to_ical())
+f.close()
+
+
