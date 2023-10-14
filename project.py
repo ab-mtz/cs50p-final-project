e56e24b523b1246da758ba53d7855c87494d1b09
ab-mtz <bmtzs@me.com>
2023-10-14 20:31:09 +0200

    Add example2.ics

--- project.py
+++ project.py
@@ -22,13 +22,17 @@ cal.add('version', '2.0')
 
 
 # Add subcomponents
+week_shifts = []
+
 event = Event()
 event.add('name', 'Schicht')
 event.add('description', 'V')
 event.add('dtstart', datetime(2023, 10, 14, 19, 10, 0, tzinfo=pytz.utc))
 event.add('dtend', datetime(2023, 10, 14, 21, 0, 0, tzinfo=pytz.utc))
 
-cal.add_component(event)
+week_shifts.append(event)
+for shift in week_shifts:
+    cal.add_component(shift)
 
 # Creatinga alarm
 
