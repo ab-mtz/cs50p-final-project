f6a373d8c4d37d27742630d5200de1090b09baed
ab-mtz <bmtzs@me.com>
2023-10-22 16:09:23 +0200

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,8 +1,8 @@
 from project.py import check_arguments
 
 def test_check_arguments():
-    args = ['project.py', 'in_file.pdf', 'out_file.ics']
+    args = ['project.py', 'in_file.pdf']
     assert check_arguments(args) == True
 
-    args = ['project.py', 'invalid.pdf', 'out_file.txt']
+    args = ['project.py', 'invalid.pdf']
     assert check_arguments(args) == False
